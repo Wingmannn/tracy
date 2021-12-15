dad0d56c2a0d6a85f6a88ac2945cdb8c9cfdb3d4
KutlayTunc <mkutlaytunc@gmail.com>
2021-12-15 18:09:59 +0300

    another fixes needed

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,8 +1,8 @@
 import io from 'socket.io-client'
 class Socket {
   constructor() {
-    // this.socket = io('http://192.168.0.11:4242')
-    this.socket = io('http://localhost:4242')
+    this.socket = io('http://192.168.0.11:4242')
+    // this.socket = io('http://localhost:4242')
 
     console.log('socket inişılayz')
   }
